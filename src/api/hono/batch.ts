d77a6154987d4c0d563bacedcc3dc5db0b3a185f
Taehee Kim <rabolution@gmail.com>
2023-07-30 16:07:50 +0900

    fix: error 처리를 미들웨어로

--- src/api/hono/batch.ts
+++ src/api/hono/batch.ts
@@ -5,16 +5,6 @@ import { parseOrderLine } from '../../typia';
 
 const batchRouter = new Hono()
 
-function withCatch(func: (ctx: Context) => Promise<Response>) {
-	return (ctx: Context) => func(ctx).catch(error => {
-		if (error instanceof Error) {
-			return Response.json({ error: error.message }, 400)
-		}
-
-		return Response.json({ error: '알 수 없는 에러' }, 400)
-	})
-}
-
 let batchList: Batch.T[] = [{
 	id: 'batch-001',
 	sku: 'SMALL-TABLE',
@@ -23,11 +13,11 @@ let batchList: Batch.T[] = [{
 	eta: null
 }]
 
-batchRouter.get('/', withCatch(async () => {
+batchRouter.get('/', async () => {
 	return Response.json({ batchList }, 200)
-}))
+})
 
-batchRouter.post('/allocate', withCatch(async (c) => {
+batchRouter.post('/allocate', async (c) => {
 	const text = await c.req.text();
 	const line = parseOrderLine(text);
 
@@ -36,6 +26,6 @@ batchRouter.post('/allocate', withCatch(async (c) => {
 	batchList = batchList.map(batch => batch.id === allocatedBatch.id ? allocatedBatch : batch);
 
 	return Response.json({ batchId: allocatedBatch.id }, 201)
-}))
+})
 
 export default batchRouter;
--- src/api/hono/index.ts
+++ src/api/hono/index.ts
@@ -6,5 +6,11 @@ const app = new Hono()
 
 app.use('*', logger())
 app.route('/batch', batchRouter)
+app.onError((error, c) => {
+    if (error instanceof Error) {
+        return Response.json({ error: error.message }, 400)
+    }
 
+    return Response.json({ error: '알 수 없는 에러' }, 400)
+  })
 export default app
