293d985e0933b0f38493b82c9414b4e874b48e0e
Taehee Kim <rabolution@gmail.com>
2023-08-26 12:15:23 +0900

    fix: fake repo에 방어적 복사 처리

--- src/persistence/FakeBatchRepo.ts
+++ src/persistence/FakeBatchRepo.ts
@@ -5,38 +5,38 @@ function FakeBatchRepo(): BatchRepo {
     let _batches: Batch.T[] = [];
 
     return {
-        async add(batch: Batch.T){
-            if (_batches.some(item => item.id === batch.id)){
+        async add(batch: Batch.T) {
+            if (_batches.some(item => item.id === batch.id)) {
                 throw Error('already exist')
             }
 
-            _batches = [..._batches, batch]
+            _batches = [..._batches, structuredClone(batch)]
         },
         async allocate(batchId, line) {
             const batch = _batches.find(item => item.id === batchId);
-            if (batch === undefined){
+            if (batch === undefined) {
                 throw Error('does not exist')
             }
-            batch.allocations.push(line)
+            batch.allocations.push(structuredClone(line))
         },
         async get(batchId: string) {
             const batch = _batches.find(item => item.id === batchId);
-            if (batch === undefined){
+            if (batch === undefined) {
                 throw Error('does not exist')
             }
             return batch;
         },
         async findBatchForOrderLine(orderId: string) {
             const batch = _batches.find(item => item.allocations.some(allocation => allocation.orderId === orderId));
-            if (batch === undefined){
+            if (batch === undefined) {
                 throw Error('does not exist')
             }
             return batch;
         },
-        async list(){
+        async list() {
             return _batches;
         }
-    }    
+    }
 }
 
 export default FakeBatchRepo;
