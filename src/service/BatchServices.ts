4dcbaaae8507ef7c4e3f03dd6c69251182bbd784
Taehee Kim <rabolution@gmail.com>
2023-08-26 12:14:21 +0900

    fix: transcation error message 강화

--- src/service/BatchServices.ts
+++ src/service/BatchServices.ts
@@ -2,14 +2,14 @@ import * as Batch from '../domain/Batch';
 import * as Allocation from '../domain/allocations';
 import { BatchUnitOfWork } from '../persistence/types';
 
-function isValidSku(sku: string, batches: Batch.T[]){
+function isValidSku(sku: string, batches: Batch.T[]) {
     return batches.some(batch => batch.sku === sku);
 }
 
-export async function allocate(line: Batch.OrderLine, uow: BatchUnitOfWork) {
+export async function allocate(line: Batch.OrderLine, uow: BatchUnitOfWork): Promise<string> {
     try {
         const batches = await uow.repo.list()
-        if (!isValidSku(line.sku, batches)){
+        if (!isValidSku(line.sku, batches)) {
             throw Error('invalid sku')
         }
         const allocatedBatch = Allocation.allocate(line, batches)
@@ -18,7 +18,10 @@ export async function allocate(line: Batch.OrderLine, uow: BatchUnitOfWork) {
         return allocatedBatch.id
     } catch (error) {
         await uow.rollback();
-        
-        throw Error('transaction failed!', { cause: error })
+
+        if (error instanceof Error) {
+            throw Error('transaction failed! \n' + error.message, { cause: error })
+        }
+        throw Error('transaction failed! \n' + error, { cause: error })
     }
 }
